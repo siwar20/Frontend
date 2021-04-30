9d2e23ad6c5f49601fa227475e040c1176a2ca3b
siwar20 <siwar.berzouga@esprit.tn>
2021-05-01 00:07:55 +0100

    Modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { AddUpdateProductComponent } from './product/add-update-product/add-update-product.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { SidbarComponent } from './home/sidbar/sidbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    AddUpdateProductComponent,
+    HomeComponent,
+    NavbarComponent,
+    SidbarComponent
   ],
   imports: [
     BrowserModule,
